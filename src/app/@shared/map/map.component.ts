19c71ad32d78c17cde90fb7e682f9320b7ab695e
Doaa Essam <doaa.essam@meng-tech.com>
2021-01-27 03:04:22 +0200

    edit lint

--- src/app/@shared/map/map.component.ts
+++ src/app/@shared/map/map.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import * as L from "leaflet";
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
-  selector: "ngx-map",
-  templateUrl: "./map.component.html",
-  styleUrls: ["./map.component.scss"],
+  selector: 'ngx-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
   bodyPopup: any;
@@ -12,12 +12,12 @@ export class MapComponent implements OnInit {
   private map;
   constructor() {
     // this.bodyPopup = `
-    // <div class="icon"><img src="imagesconn/3.svg"></div>
-    // <div class="icon"> <img src="images/3.svg"></div>
-    // <div class="icon"> <img src="images/3.svg"></div>
-    // <div class="icon"> <img src="images/1.svg"></div>
-    // <div class="icon"> <img src="images/1.svg"></div>
-    // <div class="icon"> <img src="images/1.svg"></div>
+    // <div class='icon'><img src='imagesconn/3.svg'></div>
+    // <div class='icon'> <img src='images/3.svg'></div>
+    // <div class='icon'> <img src='images/3.svg'></div>
+    // <div class='icon'> <img src='images/1.svg'></div>
+    // <div class='icon'> <img src='images/1.svg'></div>
+    // <div class='icon'> <img src='images/1.svg'></div>
     //             `;
     // this.optionsPopup = {
     //   maxWidth: '400',
@@ -27,31 +27,31 @@ export class MapComponent implements OnInit {
     // };
   }
   ngOnInit() {
-    //this.initMap(51.60706364167399, 0.22418975830078128);
-    //this.addMarker();
+     //this.initMap(51.60706364167399, 0.22418975830078128);
+     //this.addMarker();
   }
   initMap(latitude, longitude) {
-    this.map = L.map("map").setView([latitude, longitude], 18);
+    this.map = L.map('map').setView([latitude, longitude], 18);
     const tiles = L.tileLayer(
-      "http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+      'http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
       {
         maxZoom: 23,
         minZoom: 16,
-        subdomains: ["mt0", "mt1", "mt2", "mt3"],
-      }
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      },
     ).addTo(this.map);
     L.marker([latitude, longitude])
       .addTo(this.map)
       .bindPopup(this.bodyPopup, this.optionsPopup);
     // L.Icon.Default.mergeOptions({
-    //   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-    //   iconUrl: require("leaflet/dist/images/marker-icon.png"),
-    //   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+    //   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    //   iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    //   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
     // });
     tiles.addTo(this.map);
   }
   addMarker() {
-    this.map.on("click", (e) => {
+    this.map.on('click', (e) => {
       L.marker([e.latlng.lat, e.latlng.lng])
         .addTo(this.map)
         .bindPopup(this.bodyPopup, this.optionsPopup);
