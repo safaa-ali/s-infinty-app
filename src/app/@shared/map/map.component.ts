b7005a49108e94ecef00db8c94b76c8d98e5891e
Doaa Essam <doaa.essam@meng-tech.com>
2021-01-27 02:59:48 +0200

    edit linting

--- src/app/@shared/map/map.component.ts
+++ src/app/@shared/map/map.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
+import { Component, OnInit } from "@angular/core";
+import * as L from "leaflet";
 
 @Component({
-  selector: 'ngx-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss'],
+  selector: "ngx-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.scss"],
 })
 export class MapComponent implements OnInit {
   bodyPopup: any;
@@ -31,14 +31,14 @@ export class MapComponent implements OnInit {
     //this.addMarker();
   }
   initMap(latitude, longitude) {
-    this.map = L.map('map').setView([latitude, longitude], 18);
+    this.map = L.map("map").setView([latitude, longitude], 18);
     const tiles = L.tileLayer(
-      'http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+      "http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
       {
         maxZoom: 23,
         minZoom: 16,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-      },
+        subdomains: ["mt0", "mt1", "mt2", "mt3"],
+      }
     ).addTo(this.map);
     L.marker([latitude, longitude])
       .addTo(this.map)
@@ -51,7 +51,7 @@ export class MapComponent implements OnInit {
     tiles.addTo(this.map);
   }
   addMarker() {
-    this.map.on('click', (e) => {
+    this.map.on("click", (e) => {
       L.marker([e.latlng.lat, e.latlng.lng])
         .addTo(this.map)
         .bindPopup(this.bodyPopup, this.optionsPopup);
