cd23cc7a170db669b5067d7892eb765a964aa67e
Doaa Essam <doaa.essam@meng-tech.com>
2021-02-02 01:01:02 +0200

    fix lint

--- src/app/@app/map-features/map-features.module.ts
+++ src/app/@app/map-features/map-features.module.ts
@@ -1,4 +1,4 @@
-import { ThemeModule } from "./../../@theme/theme.module";
+import { ThemeModule } from './../../@theme/theme.module';
 import {
   NbMenuModule,
   NbCardModule,
@@ -8,22 +8,22 @@ import {
   NbInputModule,
   NbFormFieldModule,
   NbSidebarModule,
-} from "@nebular/theme";
-import { NgxEchartsModule } from "ngx-echarts";
+} from '@nebular/theme';
+import { NgxEchartsModule } from 'ngx-echarts';
 
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { SharedModule } from "app/@shared/shared.module";
-import { MapFeaturesRoutingModule } from "./map-features-routing.module";
-import { MapFeaturesComponent } from "./map-features.component";
-import { DocumentsComponent } from "./documents/documents.component";
-import { NgxChartsModule } from "@swimlane/ngx-charts";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'app/@shared/shared.module';
+import { MapFeaturesRoutingModule } from './map-features-routing.module';
+import { MapFeaturesComponent } from './map-features.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
-import { ImagesComponent } from "./components/images/images.component";
-import { AnalysisComponent } from "./components/analysis/analysis.component";
-import { BarChartComponent } from "./components/bar-chart/bar-chart.component";
-import { LineChartComponent } from "./components/line-chart/line-chart.component";
-import { VideosComponent } from "./components/videos/videos.component";
+import { ImagesComponent } from './components/images/images.component';
+import { AnalysisComponent } from './components/analysis/analysis.component';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
+import { LineChartComponent } from './components/line-chart/line-chart.component';
+import { VideosComponent } from './components/videos/videos.component';
 
 @NgModule({
   declarations: [
@@ -50,7 +50,7 @@ import { VideosComponent } from "./components/videos/videos.component";
     NgxChartsModule,
     NbSidebarModule.forRoot(),
     NgxEchartsModule.forRoot({
-      echarts: () => import("echarts"),
+      echarts: () => import('echarts'),
     }),
   ],
 })
