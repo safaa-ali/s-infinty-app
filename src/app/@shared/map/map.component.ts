5d1bd5db48cd56abba947e9296d1c4d5c74555c2
Doaa Essam <doaa.essam@meng-tech.com>
2021-02-03 23:30:43 +0200

    fix lint

--- src/app/@shared/map/map.component.ts
+++ src/app/@shared/map/map.component.ts
@@ -1,22 +1,29 @@
-import { Component, Input, OnInit, EventEmitter, Output, OnChanges } from '@angular/core';
-import { ProjectsService } from 'app/@app/projects/projects.service';
-import * as L from 'leaflet';
-import { icon, Layer, marker } from 'leaflet';
+import {
+  Component,
+  Input,
+  OnInit,
+  EventEmitter,
+  Output,
+  OnChanges,
+} from "@angular/core";
+import { ProjectsService } from "app/@app/projects/projects.service";
+import * as L from "leaflet";
+import { icon, Layer, marker } from "leaflet";
 @Component({
-  selector: 'ngx-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss'],
+  selector: "ngx-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.scss"],
 })
-export class MapComponent implements OnInit,OnChanges {
+export class MapComponent implements OnInit, OnChanges {
   @Input() projectId: number;
   @Output() mapAsset = new EventEmitter<any>();
   bodyPopup: any;
   optionsPopup: any;
   stationsLocations: [];
-  mapTile = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+  mapTile = L.tileLayer("http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
     maxZoom: 23,
     minZoom: 12,
-    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
   });
   map: L.Map;
   Layers: Layer[] = [this.mapTile];
@@ -37,22 +44,23 @@ export class MapComponent implements OnInit,OnChanges {
     <div class="icon"><img src='./assets/images/video.svg' id="video"></div>
     `;
     this.optionsPopup = {
-      maxWidth: '400',
-      width: '200',
+      maxWidth: "400",
+      width: "200",
       closeButton: false,
-      className: 'popupCustom',
+      className: "popupCustom",
     };
   }
-  ngOnChanges() {
-    // changes;
+  ngOnChanges(changes) {
+     changes;
     this.loadMap = false;
     this.Layers = [this.mapTile];
     this.options = {
       layers: this.Layers,
       zoom: 12,
-      center: L.latLng([47.6183869, 12.9821816])};
+      center: L.latLng([47.6183869, 12.9821816]),
+    };
     this.getLocations();
-}
+  }
 
   ngOnInit() {
     this.loadMap = false;
@@ -61,7 +69,7 @@ export class MapComponent implements OnInit,OnChanges {
   getLocations() {
     this._ProjectsService.getStations(this.projectId).subscribe((res) => {
       this.stationsLocations = res.data.items.filter(
-        (item) => item.assetType === 'stationary',
+        (item) => item.assetType === "stationary"
       );
       // console.log(this.stationsLocations);
       this.addMarkers();
@@ -73,10 +81,10 @@ export class MapComponent implements OnInit,OnChanges {
     for (let i = 0; i < this.stationsLocations.length; i++) {
       this.Layers.push(
         this.addMarker(
-          this.stationsLocations[i]['longitude'],
-          this.stationsLocations[i]['latitude'],
-          this.stationsLocations[i]['id'],
-        ),
+          this.stationsLocations[i]["longitude"],
+          this.stationsLocations[i]["latitude"],
+          this.stationsLocations[i]["id"]
+        )
       );
     }
     this.options.layers = this.Layers;
@@ -86,23 +94,23 @@ export class MapComponent implements OnInit,OnChanges {
       icon: icon({
         iconSize: [25, 41],
         iconAnchor: [13, 41],
-        iconUrl: './assets/images/marker-icon.png',
-        iconRetinaUrl: './assets/images/marker-icon.png',
-        shadowUrl: './assets/images/marker-shadow.png',
+        iconUrl: "./assets/images/marker-icon.png",
+        iconRetinaUrl: "./assets/images/marker-icon.png",
+        shadowUrl: "./assets/images/marker-shadow.png",
       }),
     })
       .bindPopup(this.bodyPopup, this.optionsPopup)
-      .on('popupopen', (e) => {
-        document.querySelector('#doc').addEventListener('click', () => {
-          this.mapAsset.emit({'type': 'documents', 'id': id});
+      .on("popupopen", (e) => {
+        document.querySelector("#doc").addEventListener("click", () => {
+          this.mapAsset.emit({ type: "documents", id: id });
         });
-        document.querySelector('#img').addEventListener('click', () => {
+        document.querySelector("#img").addEventListener("click", () => {
           e;
-          this.mapAsset.emit({'type': 'images', 'id': id});
+          this.mapAsset.emit({ type: "images", id: id });
         });
-        document.querySelector('#video').addEventListener('click', () => {
+        document.querySelector("#video").addEventListener("click", () => {
           e;
-          this.mapAsset.emit({'type': 'videos', 'id': id});
+          this.mapAsset.emit({ type: "videos", id: id });
         });
       });
     return newmarker;
