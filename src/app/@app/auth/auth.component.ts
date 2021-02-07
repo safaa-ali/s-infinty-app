9aca918386a73ee4ba6818a90491640949415d29
Ali Sbaee <alimsbaee@gmail.com>
2021-02-07 16:31:20 +0200

    Fix build configs

--- src/app/@app/auth/auth.component.ts
+++ src/app/@app/auth/auth.component.ts
@@ -1,14 +1,20 @@
 import { MENU_ITEMS } from './../../pages/pages-menu';
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthService } from 'app/@core/utils/auth.service';
 @Component({
   selector: 'ngx-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) {
+    if (authService.currentUserValue) this.router.navigate(['projects']);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
   menu = MENU_ITEMS;
 }
